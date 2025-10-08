8860eeddbf4d552027079d6e8fec2d2ae5a7d8bc
Syed Yousaf <syed.yousaf@xeventechnologies.com>
2025-10-08 19:11:40 +0500

    add static and media url

--- docapp/settings.py
+++ docapp/settings.py
@@ -131,10 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/ubuntu/django-server/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/ubuntu/django-server/media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
