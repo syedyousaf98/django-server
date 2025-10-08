f02c757e32e093dc3cc8e23d975e8d8a8230124d
syedyousafshah <68693155+syedyousaf98@users.noreply.github.com>
2025-10-08 18:18:03 +0500

    Update settings.py

--- docapp/settings.py
+++ docapp/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'xr2kc4b3cf-es5b8)&@*q#j_*2_55*1wpo(g0n#dbb3i%rsdfj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.218.84.175']
+ALLOWED_HOSTS = ['3.80.39.214']
 
 
 # Application definition
