88aaa81448c253a998e088777e3d6b908b23e248
Syed Yousaf <syed.yousaf@xeventechnologies.com>
2025-10-08 17:05:45 +0500

    remove silk code from urls

--- docapp/urls.py
+++ docapp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from debug_toolbar.toolbar import debug_toolbar_urls
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -41,3 +43,7 @@ urlpatterns = [
     path('staff/', include('staff.urls')),
     path('auth/', include('accounts.urls'))
 ] + debug_toolbar_urls()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
