9f4d71d9179891b144ddaa52963d71f8e51c77da
Syed Yousaf <syed.yousaf@xeventechnologies.com>
2025-10-13 19:14:14 +0500

    allowed host set to all

--- docapp/settings.py
+++ docapp/settings.py
@@ -33,8 +33,7 @@ SECRET_KEY = 'xr2kc4b3cf-es5b8)&@*q#j_*2_55*1wpo(g0n#dbb3i%rsdfj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['django-server.com', 'www.django-server.com', '3.225.91.173', '127.0.0.1']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django-server.com', 'www.django-server.com', '3.225.91.173', '127.0.0.1', 'localhost']
 
 # Application definition
 
